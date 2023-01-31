85bb8f5c87f90e28280bf2f7f4f33609fb0cf145
Lhy <lhy@nb.com>
2023-01-31 22:46:40 +0800

    算法笔记

--- 算法笔记练习题/10-1-1.cpp
+++ 算法笔记练习题/10-1-1.cpp
@@ -5,17 +5,7 @@ using namespace std;
 
 int main() {
     cout << 1;
-    cout << 1;
-    cout << 1;
-    cout << 1;
-    cout << 1;
-    cout << 1;
-    cout << 1;
-    cout << 1;
-    cout << 1;
-    cout << 1;
-    cout << 1;
-    cout << 1;
-    
+
+
     return 0;
 }
