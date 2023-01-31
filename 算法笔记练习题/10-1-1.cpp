77d7186435044251cb073a4e7ad98d1eebaf2619
三洋三洋 <56267907+SPLEEHYS@users.noreply.github.com>
2023-01-31 23:38:33 +0800

    Update 10-1-1.cpp

--- 算法笔记练习题/10-1-1.cpp
+++ 算法笔记练习题/10-1-1.cpp
@@ -16,6 +16,9 @@ int main() {
     cout << 1;
     cout << 1;
     cout << 1;
-    
+    cout << 2;
+    cout << 2;
+    cout << 2;cout << 2;cout << 2;cout << 2;cout << 2;cout << 2;
+    cout << 2;cout << 2;cout << 2;cout << 2;cout << 2;
     return 0;
 }
